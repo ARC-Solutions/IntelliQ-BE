329fc70fa2d70f048b3edc6e537f0de43923588a
RickyRAV <rickyraveanu02@gmail.com>
2023-10-15 23:19:31 +0200

    ITP-53 /getQuiz route implemented

--- server.js
+++ server.js
@@ -1,48 +1,87 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const OpenAI = require('openai');
 
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const app = express();
-app.use(express.json());
 
-// swagger options
+app.use(express.json());
+app.use(cors());
+
+// Swagger config
 const swaggerOptions = {
     swaggerDefinition: {
         info: {
             title: 'IntelliQ-BE API',
-            version: '1.0.0',
-            description: 'Used for IntelliQ-BE API'
-        }
+            version: '2.0.0',
+            description: 'API for IntelliQ-BE',
+        },
     },
     apis: ['server.js'],
 };
 
-// init Swagger
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-/**
- * @swagger
- * paths:
- *   /:
- *     get:
- *       tags:
- *         - General
- *       summary: 'Welcome endpoint'
- *       description: 'Returns a welcome message.'
- *       responses:
- *         200:
- *           description: 'Welcome to the IntelliQ-BE API!'
- */
 app.get('/', async (req, res) => {
     res.send('Welcome to the IntelliQ-BE API!');
 });
 
-//TODO: Prisma User & Quiz
+// Create OpenAI instance
+const openai = new OpenAI(OPENAI_API_KEY);
+app.get('/getQuiz', async (req, res) => {
+    const { interests, numberOfQuestions } = req.query;
+    const prompt = `Generate ${numberOfQuestions} multiple-choice quiz questions based on the user's interests in 
+    ${interests}. Each question should come with four answer options labeled a, b, c, and d. Include the correct answer 
+    for each question, beginning the line with 'Answer:'. 
+    The correct answer should be formatted exactly like its corresponding option.`;
 
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                'role': 'system',
+                'content': prompt
+            },
+            {
+                'role': 'user',
+                'content': `${interests}, ${numberOfQuestions} questions`
+            }
+        ],
+        temperature: 1,
+        max_tokens: 512,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    const rawContent = response.choices[0].message.content;
+    const rawQuestions = rawContent.split('\n\n');
+
+    const formattedQuestions = rawQuestions.map((q) => {
+        const parts = q.split('\n');
+        const questionText = parts[0];
+        const options = parts.slice(1, -1);
+        const correctOptionKey = parts[parts.length - 1].split(' ')[1];
+        const correctAnswer = options.find(opt => opt.startsWith(correctOptionKey));
+        return {
+            text: questionText,
+            options: options,
+            correctAnswer: correctAnswer
+        };
+    });
+
+    res.json({ questions: formattedQuestions });
+});
+
+
+
+// init jokes and start server
 const port = process.env.PORT || 3000;
 const startServer = async () => {
-
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`);
     });
