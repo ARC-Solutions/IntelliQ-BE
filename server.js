9296f339175a65bb87488fbf691d1df5839ac557
RickyRAV <rickyraveanu02@gmail.com>
2023-10-15 19:53:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ app.use(express.json());
 app.get('/', async (req, res) => {
     res.send('Welcome to the API!');
 });
+
+
+const port = process.env.PORT || 3000;
+const startServer = async () => {
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+};
+
+startServer().catch(e => console.error(e));
