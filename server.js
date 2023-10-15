8673c9fe3d90b245d8b8f513105d108231e414c4
RickyRAV <rickyraveanu02@gmail.com>
2023-10-15 19:59:27 +0200

    Swagger Docs

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 app.use(express.json());
 
+// swagger options
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'IntelliQ-BE API',
+            version: '1.0.0',
+            description: 'Used for IntelliQ-BE API'
+        }
+    },
+    apis: ['server.js'],
+};
+
+// init Swagger
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * paths:
+ *   /:
+ *     get:
+ *       tags:
+ *         - General
+ *       summary: 'Welcome endpoint'
+ *       description: 'Returns a welcome message.'
+ *       responses:
+ *         200:
+ *           description: 'Welcome to the IntelliQ-BE API!'
+ */
 app.get('/', async (req, res) => {
-    res.send('Welcome to the API!');
+    res.send('Welcome to the IntelliQ-BE API!');
 });
 
 
