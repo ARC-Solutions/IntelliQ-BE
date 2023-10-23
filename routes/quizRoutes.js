3d502258ebacab098e1dd0ca49c555aee398420c
RickyRAV <rickyraveanu02@gmail.com>
2023-10-23 22:07:16 +0200

    feat/ITP-182

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -4,13 +4,10 @@ import {isAuthenticated} from "../middlewares/isAuthenticated.js";
 export default (app) => {
     app.get('/', welcome);
     app.post('/api/quiz', isAuthenticated, getQuiz);
-    app.get('/api/quiz/javascript', getJSQuiz);
-    app.get('/api/quiz/agile', getAgileQuiz);
-    app.get('/api/quiz/anime', getAnimeQuiz);
-    app.get('/api/quiz/gaming', getGamingQuiz);
-    app.get('/api/quiz/formula-one', getFormulaOneQuiz);
-    app.get('/api/quiz/css', getCSSQuiz);
-    app.get('/test', (req, res) => {
-        res.send('Server is working');
-    });
+    app.get('/api/quiz/javascript', isAuthenticated, getJSQuiz);
+    app.get('/api/quiz/agile', isAuthenticated, getAgileQuiz);
+    app.get('/api/quiz/anime', isAuthenticated, getAnimeQuiz);
+    app.get('/api/quiz/gaming', isAuthenticated, getGamingQuiz);
+    app.get('/api/quiz/formula-one', isAuthenticated, getFormulaOneQuiz);
+    app.get('/api/quiz/css', isAuthenticated, getCSSQuiz);
 };
