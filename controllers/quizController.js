914fe543a9e1030a2174b59f01a139eece56f71b
RickyRAV <rickyraveanu02@gmail.com>
2023-11-03 18:24:10 +0100

    feat: ITP-215 ITP-169 ITP-217

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,5 +1,6 @@
 import { generateQuizQuestions } from "../services/quizService.js";
 import {prisma} from "../config/prismaClient.js";
+import {supabase} from "../config/db.js";
 
 export const welcome =  async (req, res) => {
     res.send('Welcome to the IntelliQ-BE API! For Documentation please visit: intelliq-be.azurewebsites.net/api-docs/');
@@ -12,58 +13,75 @@ export const getQuiz = async (req, res) => {
 };
 
 export const saveQuizResults = async (req, res) => {
-    const { userId, rawQuestions } = req.body;
-    const { quizTitle, questions } = rawQuestions;
+    const cookieToken = req.cookies?.token;
+    if (!cookieToken) {
+        res.status(400).json({ error: 'No token provided.' });
+        return;
+    }
 
     try {
-        // Check if quiz already exists for the user
-        let createdQuiz = await prisma.quizzes.findUnique({
-            where: {
-                user_id: userId
+        const { data: { user } } = await supabase.auth.getUser(cookieToken);
+        if (!user || !user.id) {
+            res.status(404).json({ error: 'User not found.' });
+            return;
+        }
+
+        const { rawQuestions } = req.body;
+        const { quizTitle, questions, timeTaken } = rawQuestions;
+
+        const createdQuiz = await prisma.quizzes.create({
+            data: {
+                user_id: user.id,
+                quiz_title: quizTitle,
+                total_time_taken: timeTaken,
+                correct_answers_count: 0
             }
         });
 
-        // If quiz does not exist, create a new one
-        if (!createdQuiz) {
-            createdQuiz = await prisma.quizzes.create({
+        const correctAnswersCount = questions.reduce((count, question) =>
+            count + (question.userAnswer === question.correctAnswer ? 1 : 0), 0);
+
+        const transactionOperations = questions.map(question => async () => {
+            const isCorrect = question.userAnswer === question.correctAnswer;
+            const createdQuestion = await prisma.questions.create({
                 data: {
-                    user_id: userId,
-                    quiz_title: quizTitle
+                    text: question.text,
+                    options: question.options,
+                    correct_answer: question.correctAnswer,
+                    quiz_id: createdQuiz.id
                 }
             });
-        }
 
-        // Start transaction
-        const createdQuestions = await prisma.$transaction(
-            questions.map(question =>
-                prisma.questions.create({
-                    data: {
-                        text: question.text,
-                        options: question.options,
-                        correct_answer: question.correctAnswer,
-                        quiz_id: createdQuiz.id
-                    }
-                })
-            )
-        );
+            return prisma.user_responses.create({
+                data: {
+                    question_id: createdQuestion.id,
+                    quiz_id: createdQuiz.id,
+                    user_answer: question.userAnswer,
+                    is_correct: isCorrect
+                }
+            });
+        });
 
-        // After creating questions, create responses with the new question IDs
-        const createdResponses = await prisma.$transaction(
-            createdQuestions.map((createdQuestion, index) => {
-                const correspondingUserQuestion = questions[index];
-                return prisma.user_responses.create({
-                    data: {
-                        question_id: createdQuestion.id,
-                        quiz_id: createdQuiz.id,
-                        user_answer: correspondingUserQuestion.userAnswer,
-                        time_taken: correspondingUserQuestion.timeTaken,
-                        is_correct: correspondingUserQuestion.userAnswer === correspondingUserQuestion.correctAnswer
-                    }
-                });
-            })
-        );
+        await Promise.all(transactionOperations.map(operation => operation()));
 
-        res.status(201).json({ message: 'Quiz results saved successfully.', createdQuestions, createdResponses });
+        await prisma.quizzes.update({
+            where: { id: createdQuiz.id },
+            data: { correct_answers_count: correctAnswersCount }
+        });
+
+        const formattedResponse = {
+            quiz_id: createdQuiz.id,
+            rawQuestions: {
+                quiz_title: quizTitle,
+                questions: questions.map(question => ({
+                    text: question.text,
+                    correctAnswer: question.correctAnswer,
+                    userAnswer: question.userAnswer
+                })),
+            },
+        };
+
+        res.status(201).json(formattedResponse);
     } catch (error) {
         console.error('Error saving quiz data:', error);
         res.status(500).json({ error: 'An error occurred while saving quiz data.' });
