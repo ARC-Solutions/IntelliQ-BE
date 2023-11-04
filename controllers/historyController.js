1ef0ff191c2aa614bcc86cb34d628f5acb0f0dda
RickyRAV <rickyraveanu02@gmail.com>
2023-11-04 21:56:20 +0100

    feat: ITP-225

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -3,17 +3,30 @@ import {prisma} from "../config/prismaClient.js";
 export const userHistory = async (req, res) => {
     const { user: { id: user_id } } = req.user;
 
+    // Retrieve offset and limit from the query parameters, and provide default values if they are not provided
+    const offset = parseInt(req.query.offset, 10) || 0;
+    const limit = parseInt(req.query.limit, 10) || 20; // default limit to 10 if not specified
+
     try {
         const quizzes = await prisma.quizzes.findMany({
             where: { user_id },
+            skip: offset, // skip a certain number of records
+            take: limit, // take a certain number of records
+            orderBy: { created_at: 'desc' },
             select: {
                 id: true,
                 quiz_title: true,
                 total_time_taken: true
             }
         });
-        res.json(quizzes);
+
+        // Also return the total count of records for the frontend to calculate total pages
+        const totalCount = await prisma.quizzes.count({ where: { user_id } });
+
+        res.json({ quizzes, totalCount });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+
+
