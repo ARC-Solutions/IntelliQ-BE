ffb88eef2e7b62166006b86869dbe34fb0c37569
RickyRAV <rickyraveanu02@gmail.com>
2024-10-13 12:57:38 +0200

    Update swagger.js

--- config/swagger.js
+++ config/swagger.js
@@ -1,7 +1,7 @@
 import swaggerUi from "swagger-ui-express";
 import YAML from 'yamljs';
 
-const swaggerDocument = YAML.load('./services/swaggerDefinitions.yaml');
+const swaggerDocument = YAML.load('./services/swaggerDefinitions.YAML');
 
 export const configureSwagger = (app) => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
