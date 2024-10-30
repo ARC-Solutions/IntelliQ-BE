60001d59a143c3874135ee9b512fb13c4ed6cf5a
RickyRAV <rickyraveanu02@gmail.com>
2024-10-30 13:49:57 +0100

    Update configureMiddlewares.js

--- config/configureMiddlewares.js
+++ config/configureMiddlewares.js
@@ -10,7 +10,7 @@ export const configureMiddlewares = (app) => {
     app.use(express.json());
     app.use(cors({
         origin: function (origin, callback) {
-            const allowedOrigins = ['http://localhost:3001', 'https://www.intelliq.arc-solutions.xyz', 'https://www.intelliq.dev', 'https://www.beta.intelliq.dev', 'https://www.internal.intelliq.dev', 'https://intelliq-be-s1.azurewebsites.net', 'https://intelliq-be-s2.azurewebsites.net', 'https://intelliq-be-s3.azurewebsites.net'];
+            const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://www.intelliq.arc-solutions.xyz', 'https://www.intelliq.dev', 'https://www.beta.intelliq.dev', 'https://www.internal.intelliq.dev', 'https://intelliq-be-s1.azurewebsites.net', 'https://intelliq-be-s2.azurewebsites.net', 'https://intelliq-be-s3.azurewebsites.net'];
             if (!origin || allowedOrigins.indexOf(origin) !== -1) {
                 callback(null, true);
             } else {
