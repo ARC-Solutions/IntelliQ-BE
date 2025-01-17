d6bcdab55c41f522d00e41acabee937e320bb3c6
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-17 13:53:00 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const dbURL = process.env.SUPABASE_URL;
-const annonKEY = process.env.DATABASE_ANON_KEY;
-export const supabase = createClient(dbURL, annonKEY);
+const annonKEY = process.env.SUPABASE_ANON_KEY;
+export const supabase = createClient(dbURL, annonKEY, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
 //console.log(supabase);
