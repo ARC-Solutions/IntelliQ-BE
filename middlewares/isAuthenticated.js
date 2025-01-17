2eb22461a0e2201158d1a8200dc74977a5776a83
RickyRAV <101335214+RickyRAV@users.noreply.github.com>
2025-01-17 13:38:28 +0100

    gbnfsbfsd

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,25 +1,25 @@
 import { supabase } from "../config/db.js";
 
 export const isAuthenticated = async (req, res, next) => {
-    // Read from Authorization header
-    const headerToken = req.headers['Authorization']?.split(' ')[1];
+  // Read from Authorization header
+  const headerToken = req.headers["authorization"]?.split(" ")[1];
 
-    // Read from cookies
-    const cookieToken = req.cookies?.token;
-    //console.log(cookieToken);
+  // Read from cookies
+  const cookieToken = req.cookies?.token;
+  //console.log(cookieToken);
 
-    // Use the first available token
-    const token = headerToken || cookieToken;
-    // console.log(token);
-    if (!token) {
-        return res.status(401).json({ error: 'Not authorized' });
-    }
+  // Use the first available token
+  const token = headerToken || cookieToken;
+  // console.log(token);
+  if (!token) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
 
-    const { data: user, error } = await supabase.auth.getUser(token);
-    if (error || !user) {
-        return res.status(401).json({ error: 'Not authorized' });
-    }
+  const { data: user, error } = await supabase.auth.getUser(token);
+  if (error || !user) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
 
-    req.user = user;
-    next();
+  req.user = user;
+  next();
 };
